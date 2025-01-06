a049f45b62feb268ed49d67b6b77011ad675c9fc
alexmendum <alexmendum@gmail.com>
2025-01-05 17:51:23 -0800

    added collision and asteroid deletion

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def main():
     
     dt = 0
 
+    #Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -40,6 +41,13 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
+
 
         screen.fill("black")        
         
