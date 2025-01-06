6671cf90155d7b472c4e5dc0832dfe2508b21678
alexmendum <alexmendum@gmail.com>
2025-01-05 17:39:56 -0800

    add shoot method

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -18,9 +19,6 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -32,7 +30,16 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
